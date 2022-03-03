37ed29191329ec98a8912fa2071082f34138f503
kindao11 <99205614+kindao11@users.noreply.github.com>
2022-03-04 02:30:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,8 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 git clone https://github.com/CHN-beta/xmurp-ua.git package/xmurp-ua
 git clone https://github.com/chenhaowen01/gdut-drcom-for-openwrt.git package/gdut-drcom
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
